35f3e5c3e1ce233504faac8fa8ac1a893ab3b6a8
RachyHassan <tolulopehassan97@gmail.com>
2023-10-05 19:19:05 +0100

    pycode

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-from add_0 import add
-a = 1
-b = 2
-end = add(a, b)
-print("{} + {} = {}".format(a, b, end))
+if __name__ == "__main__":
+    from add_0 import add
+    a = 1
+    b = 2
+    end = add(a, b)
+    print("{} + {} = {}".format(a, b, end))
