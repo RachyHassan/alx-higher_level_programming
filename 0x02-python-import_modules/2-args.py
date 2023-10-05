fcb0d761df146575b855b5a88932f0c5f5c1b1a7
RachyHassan <tolulopehassan97@gmail.com>
2023-10-05 22:29:26 +0100

    argument

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,11 +3,14 @@
 if __name__ == "__main__":
     import sys
     calc = len(sys.argv) - 1
+
     if calc == 0:
-        print("0 arguments")
+        print("0 arguments.")
     elif calc == 1:
-        print("1 argument")
+        print("1 argument:")
+        for c in range(calc):
+            print("{}: {}".format(c + 1, sys.argv[c + 1]))
     else:
-        print("{} arguments".format(calc))
+        print("{} arguments:".format(calc))
         for c in range(calc):
             print("{}: {}".format(c + 1, sys.argv[c + 1]))
