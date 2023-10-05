4ef5e0dd30a0a490e2a725ad50470d5b1857fae7
RachyHassan <tolulopehassan97@gmail.com>
2023-10-05 19:33:05 +0100

    pycode

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+    import sys
+    calc = len(sys.argv) - 1
+    if calc == 0:
+        print("0 arguments")
+    elif calc == 1:
+        print("1 argument")
+    else:
+        print("{} arguments".format(calc))
+        for c in range(calc):
+            print("{}: {}".format(c + 1, sys.argv[c + 1]))
