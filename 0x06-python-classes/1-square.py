0ca90f9f9bb054eebda579cd1f585d2a282f252d
RachyHassan <tolulopehassan97@gmail.com>
2023-10-25 16:43:24 +0100

    oop

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
+"""A square module to define the Square class."""
+
 
 class Square:
+    """ A class to represent a Square. """
     def __init__(self, size):
+        """Initialize a new instance 'size'. """
         self.__size = size
--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,12 +1,16 @@
 #!/usr/bin/python3
+"""A square module to define the Square class."""
+
 
 class Square:
+    """ A class to represent a Square."""
     def __init__(self, size=0):
+        """ Initialize the instance size"""
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
         elif size < 0:
             raise ValueError("Size must be >= 0")
-        self.__size = size
+    self.__size = size
 
     def area(self):
         return (self.__size * self.__size)
