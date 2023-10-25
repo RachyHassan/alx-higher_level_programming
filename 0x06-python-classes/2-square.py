6fd687962ae9b76cf2cfcb5563a72022da2bcc21
RachyHassan <tolulopehassan97@gmail.com>
2023-10-25 16:46:50 +0100

    oop

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -11,4 +11,4 @@ class Square:
             raise TypeError("Size must be an integer")
         elif size < 0:
             raise ValueError("Size must be >= 0")
-    self.__size = size
+        self.__size = size
--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -10,7 +10,7 @@ class Square:
             raise TypeError("size must be an integer")
         elif size < 0:
             raise ValueError("Size must be >= 0")
-    self.__size = size
+        self.__size = size
 
     def area(self):
         return (self.__size * self.__size)
