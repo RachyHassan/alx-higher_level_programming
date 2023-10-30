e362dda28207a0927d48d4941f3e50aaf23b960d
RachyHassan <tolulopehassan97@gmail.com>
2023-10-30 13:14:35 +0100

    oop

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+""" A module to define a rectangle class. """
+
+
+class Rectangle:
+    """ A class to represent a rectangle. """
+    pass
---
+++ 0x08-python-more_classes/README.md
@@ -0,0 +1,1 @@
+0x08. Python - More Classes and Objects. More on object-oriented programming, attributes, methods, init-method and all the good stuff
