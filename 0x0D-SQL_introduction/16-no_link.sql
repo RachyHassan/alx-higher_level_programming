7663218c6226d6672a74c81a7cf8306cf3825aa9
RachyHassan <tolulopehassan97@gmail.com>
2023-12-14 12:49:23 +0100

    sql

--- 0x0D-SQL_introduction/16-no_link.sql
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -1,5 +1,5 @@
 -- A script that lists all records of the second_table having a name value.
-SELECT 'score', 'name'
-FROM 'second_table'
-WHERE 'name' != ""
-ORDER BY 'score' DESC;
+SELECT `score`, `name`
+FROM `second_table`
+WHERE `name` != ""
+ORDER BY `score` DESC;
