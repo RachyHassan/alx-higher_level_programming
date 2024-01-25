697eb22022be7913e67d6403d9f627c0f2ef2017
RachyHassan <tolulopehassan97@gmail.com>
2024-01-26 00:50:22 +0100

    corrected

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # A bash script that takes in a URL, and sends a request..
-curl -sI "$1" | grep -i 'content-length' | awk '{print $2}' | tr -d '\r')
+curl -sI "$1" | grep -i 'content-length' | awk '{print $2}' | tr -d '\r'
