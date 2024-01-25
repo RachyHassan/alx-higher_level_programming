9f8c35f6ca77ba22ecdbe90b9751bf3c7682484a
RachyHassan <tolulopehassan97@gmail.com>
2024-01-26 00:49:21 +0100

    corrected

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,17 +1,3 @@
-#!/usr/bin/bash
-# A bash script that takes in a URL, sends a request to the
-# URL, and displays the size of the body of the response.
-
-if [ -z "$1" ]; then
-	echo "Usage: $0 <URL>"
-	exit 1
-fi
-
-url=$1
-size=$(curl -sI "$url" | grep -i 'content-length' | awk '{print $2}' | tr -d '\r')
-
-if [ -z "$size" ]; then
-	echo "Can't determine the size of the response body for $url."
-else
-	echo "${size}"
-fi
+#!/bin/bash
+# A bash script that takes in a URL, and sends a request..
+curl -sI "$1" | grep -i 'content-length' | awk '{print $2}' | tr -d '\r')
