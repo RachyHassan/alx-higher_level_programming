cde1928f747e2c9ad7db59bad8092f11b38d8da4
RachyHassan <tolulopehassan97@gmail.com>
2024-01-26 01:15:26 +0100

    1-body.sh

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # A script that takes in a URL, sends a GET request and displays the body.
-curl -s -w "%{http_code}" -L "$1" | awk '/^200/{getline; print}'
+curl -s -w "%{http_code}" -L "$1" | awk '{getline; print}'
