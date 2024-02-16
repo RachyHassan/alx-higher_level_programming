957f0e2d0d85b671f79ea0c1bee04afdae4fc585
RachyHassan <tolulopehassan97@gmail.com>
2024-02-16 15:00:32 +0100

    documentatin

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -3,7 +3,11 @@
 import urllib.request
 import sys
 
+# fetch the url from command line
 url = sys.argv[1]
+# send a request to url and retrieve a response
 with urllib.request.urlopen(url) as response:
+# extract the value of X-Request-Id
     x_request_id = response.getheader('X-Request-Id')
+# print variable
 print(x_request_id)
