6e8bfef884846f927789c1b353be5021659f16e9
RachyHassan <tolulopehassan97@gmail.com>
2024-02-17 07:25:19 +0100

    pycode

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -11,8 +11,12 @@ import sys
 def post_request(url, email):
     """
     A function to encode email parameter.
+    parameters:
+        url (str): The URLto send the POST request
+        email (str): email included in the parameter.
     """
     data = urllib.parse.urlencode({'email': email}).encode('utf-8')
     with urllib.request.urlopen(url, data=data) as response:
         body = response.read().decode('utf-8')
-        print(body)
+
+    print(body)
