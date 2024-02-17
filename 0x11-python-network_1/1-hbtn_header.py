d9e6ab63af7fbed55af1ce6d764eeb6308db9eb6
RachyHassan <tolulopehassan97@gmail.com>
2024-02-17 07:03:58 +0100

    module

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,14 +1,15 @@
 #!/usr/bin/python3
-# A script that takes in a URL, sends a request and displays response value
+"""
+A script that takes in a URL, sends a request and displays response value
+"""
 import urllib.request
 import sys
 
 # fetch the url from command line
-# send a request to url and retrieve a response
-# extract the value of X-Request-Id
-# print variable
 url = sys.argv[1]
-
+# send a request to url and retrieve a response
 with urllib.request.urlopen(url) as response:
+    # extract the value of X-Request-Id
     x_request_id = response.getheader('X-Request-Id')
+# print variable
 print(x_request_id)
---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+# A script that takes in a URL and an email, sends a Post request
+# and displays the body of the response.
+
